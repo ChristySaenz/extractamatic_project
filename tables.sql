d69751a25067d69f832a11f85e33053c29592611
alliecarlile <allie.carlile@yahoo.com>
2022-09-15 20:57:41 -0500

    queries

--- tables.sql
+++ tables.sql
@@ -1,12 +1,12 @@
-DROP TABLE clean_waffles;
+DROP TABLE clean_waffles cascade;
 
 
 CREATE TABLE clean_waffles (
 	"," VARCHAR NOT NULL,
 	"state" VARCHAR NOT NULL,
 	"Loc" VARCHAR NOT NULL,
-	"% Married" DECIMAL NOT NULL,
-	"% Divorced" DECIMAL NOT NULL,
+	"%_Married" DECIMAL NOT NULL,
+	"%_Divorced" DECIMAL NOT NULL,
 	"WaffleHouses" VARCHAR NOT NULL,
 	PRIMARY KEY ("state")
 );
@@ -15,9 +15,16 @@ DROP TABLE clean_gunviol;
 
 CREATE TABLE clean_gunviol (
 	"," VARCHAR NOT NULL,
+	"date" VARCHAR NOT NULL,
 	"state" VARCHAR NOT NULL,
 	"n_killed" VARCHAR NOT NULL,
 	"n_injured" VARCHAR NOT NULL,
-	"year" VARCHAR NOT NULL,
 	FOREIGN KEY (state) REFERENCES clean_waffles(state)
 );
+
+SELECT * FROM clean_waffles;
+
+SELECT * FROM clean_gunviol;
+
+SELECT * FROM clean_waffles FULL OUTER JOIN clean_gunviol
+   ON clean_waffles.state = clean_gunviol.state;
